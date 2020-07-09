08b210709d27c0603236de9569faab1eadb8c17d
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-07-09 10:51:06 +0200

    Installer update
    Install all prerequires

---
+++ install_prerequires.sh
@@ -0,0 +1,8 @@
+pip install numpy
+pip install random
+pip install cython
+pip install sphinx
+pip install numpydoc
+pip install sphinx-autoapi
+pip install sphinx-automodapi
+pip install sphinx-rtd-theme
