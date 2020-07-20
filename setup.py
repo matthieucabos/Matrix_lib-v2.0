3e0b5d558b51354c80130873ef9403f370e31f05
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-07-20 11:43:23 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
+import os
+os.system('.\\install_prerequires.sh')
+os.system('.\\install_prerequires.bat')
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-import os
 
 ext_modules=[
 		Extension("cmatrix",
 				sources=["cmatrix.pyx"],
 				extra_compile_args=['-fopenmp'],
 				extra_link_args=['-fopenmp'])]
-os.system('.\\install_prerequires.sh')
-os.system('.\\install_prerequires.bat')
 setup(
 	name="cmatrix",
 	cmdclass = {'build_ext' : build_ext},
