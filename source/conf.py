8897eb4e9a8d9d5eae90439e61c9ad26d6a022c0
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-07-20 11:33:09 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,8 @@
 import sys
 import os
 sys.path.insert(0,os.path.abspath('..'))
-
+os.system('..\\install_prerequires.sh')
+os.system('..\\install_prerequires.bat')
 
 # -- Project information -----------------------------------------------------
 
