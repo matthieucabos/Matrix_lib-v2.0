caf2ffbf805ca019b0f74ac83be0f27320ea78e9
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-07-20 11:37:58 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os
 
 ext_modules=[
 		Extension("cmatrix",
 				sources=["cmatrix.pyx"],
 				extra_compile_args=['-fopenmp'],
 				extra_link_args=['-fopenmp'])]
-
+os.system('.\\install_prerequires.sh')
+os.system('.\\install_prerequires.bat')
 setup(
 	name="cmatrix",
 	cmdclass = {'build_ext' : build_ext},
@@ -17,4 +19,4 @@ setup(
 
 # python3 setup.py build_ext --inplace
 #num_threads=os.cpu_count()
-#for i in prange(100, nogil=True, num_threads):
+#for i in prange(100, nogil=True, num_threads=4):
